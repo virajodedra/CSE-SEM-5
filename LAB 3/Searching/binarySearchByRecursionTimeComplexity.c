8cf9bcf37fa07e9bff47e5ead70f414314d306db
Odedra Viraj <virajodedra1808@gmail.com>
2025-07-01 08:44:05 +0530

    Create binarySearchByRecursionTimeComplexity.c

---
+++ LAB 3/Searching/binarySearchByRecursionTimeComplexity.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<time.h>
+
+
+bool binarySearch(int arr[], int n,  int key, int start, int end) {
+    
+    
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == key){
+            return true;
+        }
+        else if(arr[mid] > key) {
+            binarySearch(arr, n, key, start, mid - 1);
+        }
+        else {
+            binarySearch(arr, n, key, mid + 1, end);
+        }
+    }
+
+    return false;
+}
+
+void main() {
+    int key;
+    printf("Enter the key to find in the array :");
+    key  = scanf("%d", &key);
+
+    // int arr[n];
+    
+    FILE *fp;
+    clock_t start, end;
+    double cpu_time_used;
+    int arr[100000];
+    int n = 100000;
+
+
+    fp = fopen("./best_for_100000.txt", "r");
+    for(int i = 0; i < n; i++){
+        fscanf(fp, "%d", &arr[i]);
+    }
+    fclose(fp);
+    start = clock();
+    bool result  = binarySearch(arr, n, key, 0, n - 1);
+    end = clock();
+    cpu_time_used = ((double) (end - start))/CLOCKS_PER_SEC;
+    if(result){
+        printf("Find the key successfully !!!");
+    }
+    printf("TIME TAKEN TO FIND THE KEY IN  THE ARRAY IS : %f seconds\n\n", cpu_time_used);
+
+
+}
